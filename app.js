b3cd5deae2021e3754edcfb5eaa4386f7a5a6e32
janalam <janalam1224@gmail.com>
2025-05-15 16:55:06 +0500

    app.js modified

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ const express = require('express');
 const authRouter = require('./routes/authRouter');
 const userRouter = require('./routes/userRouter');
 const productRouter = require('./routes/productRouter');
-const admin = require('./config/firebaseAdmin');
 
 const app = express();
 
